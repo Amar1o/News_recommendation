28a54f60068a9fced3702070dcdb9f9e90280e5a
Amar1o <msm.amarali.03@gmail.com>
2024-11-11 19:54:35 +0530

    attempt at understanding categorizations using stanfordnlp

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -3,6 +3,7 @@ module org.example.news_recommendation {
     requires javafx.fxml;
     requires java.sql;
     requires com.google.gson;
+    requires stanford.corenlp;
 
 
     opens org.example.news_recommendation to javafx.fxml;
